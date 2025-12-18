e202db397765b722bcb662f72a4f37ae68bd6b31
LocaCele04 <c.locatelli60@studenti.unibg.it>
2025-12-18 14:50:26 +0100

    Junit

---
+++ ABLV_IngSW/src/main/java/client/Rettangolo.java
@@ -0,0 +1,20 @@
+package client;
+
+public class Rettangolo {
+	private int base;
+	private int altezza;
+	public Rettangolo(int b, int a){
+	base=b;
+	altezza=a;
+	}
+	public int getBase(){
+	return base;
+	}
+	public int getAltezza(){
+	return altezza;
+	}
+	public int getArea(){
+	return altezza+base;
+	}
+
+}
