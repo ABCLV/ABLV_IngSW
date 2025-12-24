5d0a9c75ef71de5dff5d3caf5eaef81bbe3c9e37
LocaCele04 <c.locatelli60@studenti.unibg.it>
2025-12-24 16:50:24 +0100

    Popolazione db di partenza
    alcune tabelle sono rimaste vuote come le iscrizioni e le partecipazioni perchè il sw inizierà a registrare le gare del 2026

--- ABLV_IngSW/src/main/java/database/PopolaDB.java
+++ ABLV_IngSW/src/main/java/database/PopolaDB.java
@@ -26,7 +26,7 @@ public class PopolaDB {
         	//importGara();
         	
         	//importTurno();
-        	importIscrive();
+        	//importIscrive();
             
             
             
